e52e4bd3559dc54ab28346a63573a4002b175782
decaduto <78024779+decaduto@users.noreply.github.com>
2021-03-18 20:02:49 +0100

    Create Ctxs.h

---
+++ Ctxs.h
@@ -0,0 +1,14 @@
+#ifndef __CTXS_H
+#define __CTXS_H
+
+// Made by Edoardo Mantovani, 2020
+// import libraries
+
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdint.h>
+#endif
